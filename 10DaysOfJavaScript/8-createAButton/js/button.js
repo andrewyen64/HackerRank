b9445b0bbfcac769cf65b12c0d9108734b2245cf
Andrew <andrewyen64@gmail.com>
2020-11-28 21:13:57 -0800

    Fix name and add shorter solution

---
+++ 10DaysOfJavaScript/8-createAButton/js/button.js
@@ -0,0 +1,15 @@
+// The button's initial text label is 0. After each click, the button must increment by 1. 
+// Recall that the button's text label is the JS object's innerHTML property.
+var btn = document.getElementById("btn");
+
+// Using addEventListener
+// btn.addEventListener("click", function() {
+//     var count = btn.innerHTML;
+//     count++;
+//     btn.innerHTML = count;
+// })
+
+// Shorter Solution using onClick
+btn.onclick = function() {
+    btn.innerHTML++;
+}
